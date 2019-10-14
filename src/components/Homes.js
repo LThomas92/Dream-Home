37ae4a50e6bdca85b0a50ad9054e9f8c472a1b50
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-10-14 14:34:27 -0400

    Removed Inactive Page Pagination

--- src/components/Homes.js
+++ src/components/Homes.js
@@ -62,18 +62,6 @@ class Homes extends React.Component {
             return this.renderHomes(home);
           })}
         </div>
-
-        <section className="page-pagination-container">
-          <ul className="page-pagination">
-            <li>Prev</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>Next</li>
-          </ul>
-        </section>
       </div>
     );
   }
